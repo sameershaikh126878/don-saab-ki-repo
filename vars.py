04a1be08a7c5a1ee556b64e6e0c970be951fb323
sameershaikh126878 <ywallah6@gmail.com>
2025-04-12 19:50:22 +0530

    Delete vars.py

--- vars.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BOT_TOKEN = os.environ.get("8188509235:AAGtp4-ZQ1JHhzCdYM0eI5HK4LbPSo5uNog")
-API_ID = os.environ.get("17950436")
-API_HASH = os.environ.get("9d8ae4d35b1106df910b94dcaf73d346")
-
